e7e71550d01c721b64e6b2a4fd137427a552c667
Andrey Dmitriev <andrydmit@gmail.com>
2019-10-04 17:09:31 +0300

    add component structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const el = <h1>Hello</h1>
+const ToDOList = () => {
 
-ReactDom.render(el, document.getElementById('root'))
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );
+}
+
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  )
+}
+
+const SearchPanel = () =>  <input placeholder="search" />
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <ToDOList />
+    </div>
+  )
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
